2c0c8f4222169211bbce957f6a2a5b07e1ca5368
Abdul Rafey <abdulrafey@Unknown.gateway>
2023-07-08 17:51:15 +0500

    changed Player.name to Player.id'

--- player.py
+++ player.py
@@ -6,13 +6,13 @@ from ranking import HandRanker
 
 class Player:
 
-    def __init__(self, name: str, hand: Hand) -> None:
-        self.name = name
+    def __init__(self, id: str, hand: Hand) -> None:
+        self.id = id
         self.hand = hand
         self.rank = self._rank_hand()
 
     def __repr__(self) -> str:
-        return self.name
+        return self.id
 
     def _rank_hand(self) -> int:
         return HandRanker.rank(self.hand)
@@ -21,9 +21,9 @@ class Player:
     def from_dict(cls, player_dict: Dict) -> 'Player':
 
         try:
-            name = player_dict['name']
+            id = player_dict['id']
             hand = Hand.from_string(player_dict['hand'])
         except KeyError:
-            raise Exception(f"Invalid player_dict format, expecting ['name', 'hand'] keys but got {list(player_dict.keys())}")
+            raise Exception(f"Invalid player_dict format, expecting ['id', 'hand'] keys but got {list(player_dict.keys())}")
         
-        return Player(name, hand)
+        return Player(id, hand)
